f13b13369401439fbb1a896d26d4f42f91f590e3
Gauransh kate <kategauransh@users.noreply.github.com>
2026-07-26 20:31:47 +0530

    style: adjust spacing and trailing newlines in LedgerEntryRepository.java

--- src/main/java/com/gauransh/ledger/repository/LedgerEntryRepository.java
+++ src/main/java/com/gauransh/ledger/repository/LedgerEntryRepository.java
@@ -1,9 +1,8 @@
-/**
- * Source documentation and metadata properties.
- */
 package com.gauransh.ledger.repository;
 
 import com.gauransh.ledger.model.LedgerEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LedgerEntryRepository extends JpaRepository<LedgerEntry, Long> {}
+
+// EOF (End of File checkstyle directive)

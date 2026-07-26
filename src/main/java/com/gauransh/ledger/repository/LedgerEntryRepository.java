4f3e5af12546251d0c55a955c0fc448c2f55e1f0
Gauransh kate <kategauransh@users.noreply.github.com>
2026-07-26 20:25:24 +0530

    feat: create ThreadLocal TenantContext propagation for multi-tenancy context

--- src/main/java/com/gauransh/ledger/repository/LedgerEntryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gauransh.ledger.repository;
-
-import com.gauransh.ledger.model.LedgerEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LedgerEntryRepository extends JpaRepository<LedgerEntry, Long> {}
--- src/main/java/com/gauransh/ledger/repository/WalletRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gauransh.ledger.repository;
-
-import com.gauransh.ledger.model.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet, Long> {}
